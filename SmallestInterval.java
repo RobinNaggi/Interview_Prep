db73c3a6d602ce1640052e54ac554c72449e9215
RobinNaggi <43588161+RobinNaggi@users.noreply.github.com>
2019-12-13 16:37:19 -0800

    Update SmallestInterval.java

--- SmallestInterval.java
+++ SmallestInterval.java
@@ -14,4 +14,55 @@
 */
 
 
+import java.util.Set;
+import java.util.HashSet;
 
+public class SmallestInterval {
+
+    public static void main(String []args){
+        
+        Set<int[]> closedIntervalsSetList = makeIntervals();
+        
+        int[] result = findSmallestClosedInterval(closedIntervalsSetList);
+        System.out.println("["+ result[0] + ", "+ result[1] + "]");
+    }
+    
+    private static int[] findSmallestClosedInterval(Set<int[]> closedIntervalsSetList){
+        
+        
+        int minEndPoint = Integer.MAX_VALUE;
+        int maxStartingPoint = Integer.MIN_VALUE;
+        
+        for(int[] closedIntervals : closedIntervalsSetList){
+            int currentStart = closedIntervals[0];
+            int currentEnd = closedIntervals[1];
+            
+            minEndPoint = Math.min(minEndPoint, currentEnd);
+            maxStartingPoint = Math.max(maxStartingPoint, currentStart);
+        }
+        
+        int[] result = {minEndPoint, maxStartingPoint};
+        return result;
+        
+    }
+    
+    // [0, 3], [2, 6], [3, 4], [6, 9] 
+    private static Set<int[]> makeIntervals(){
+        
+        Set<int[]> closedIntervalsList = new HashSet<int[]>();
+        
+        int[] interval1 = {0, 3};
+        closedIntervalsList.add(interval1);
+        
+        int[] interval2 = {2, 6};
+        closedIntervalsList.add(interval2);
+        
+        int[] interval3 = {3, 4};
+        closedIntervalsList.add(interval3);
+        
+        int[] interval4 = {6, 9};
+        closedIntervalsList.add(interval4);
+        
+        return closedIntervalsList;
+    }
+}
