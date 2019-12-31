fee93f0daf285cf2ed0e4c682ae88030278a1bde
RobinNaggi <43588161+RobinNaggi@users.noreply.github.com>
2019-12-31 09:43:56 -0800

    Update FindTwoElements.java

--- FindTwoElements.java
+++ FindTwoElements.java
@@ -1,2 +1,17 @@
 /*
+Given an array of integers in which two elements appear 
+exactly once and all other elements appear exactly twice, 
+find the two elements that appear only once.
+
+For example, given the array [2, 4, 6, 8, 10, 2, 6, 10], 
+return 4 and 8. The order does not matter.
+*/
+
+/*
+  1.
+      Time: O(n) 
+      Space: O(n)
+  2. 
+      Time: O(nlog(n)) 
+      Space: O(1)
 */
