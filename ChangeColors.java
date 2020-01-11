34e78c37c333ccf18e2cfb215e45720b8329d7c6
RobinNaggi <43588161+RobinNaggi@users.noreply.github.com>
2020-01-11 11:46:17 -0800

    Update ChangeColors.java

--- ChangeColors.java
+++ ChangeColors.java
@@ -19,6 +19,62 @@ B B B
 */
 
 /*
-  Time:   O()
-  Space:  O()
+  Time:   O(row * col)
+  Space:  O(row * col)
 */
+
+public class ChangeColors{
+    
+    public static void main(String []args){
+        String[][] colorImage = { { "B", "B", "W"},
+                       { "W", "W", "W"},
+                       { "W", "W", "W"},
+                       { "B", "B", "B"}};
+                       
+        printMatrix(colorImage);
+  
+        changeColor(colorImage, 2, 2, colorImage[2][2], "G");
+        
+        printMatrix(colorImage);
+    }
+    
+    private static void changeColor(String[][] colorImage, int row, int col, String oldColor, String newColor){
+        //index out of bounds check of rows
+        if(row < 0 || row >= colorImage.length){
+            return;
+        }
+        //index out of bounds check of col
+        if(col < 0 || col >= colorImage[row].length){
+            return;
+        }
+        //return if the I have already changed the color
+        if(colorImage[row][col].equals(newColor)){
+            return;
+        }
+        // return if its a different color then we care about
+        if(!colorImage[row][col].equals(newColor) && !colorImage[row][col].equals(oldColor) ){
+            return;
+        }
+        //change the current color to the new if it 
+        //matches the one we care and DFS the rest on the matrix
+        if(colorImage[row][col].equals(oldColor)){
+            colorImage[row][col] = newColor;
+            changeColor(colorImage, row-1, col, oldColor, newColor);
+            changeColor(colorImage, row, col+1, oldColor, newColor);
+            changeColor(colorImage, row+1, col, oldColor, newColor);
+            changeColor(colorImage, row, col-1, oldColor, newColor);
+        }
+    }
+    
+    private static void printMatrix(String[][] colorImage){
+        System.out.println();
+        for(int i = 0; i < colorImage.length; i++){
+            for(int j = 0; j < colorImage[i].length; j++){
+                System.out.print(colorImage[i][j]+ " ");
+            }
+            System.out.println();
+        }
+    }
+    
+    
+}
