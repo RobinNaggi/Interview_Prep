7b17b71850de4b4118f04e14710b406650d9c9f0
RobinNaggi <43588161+RobinNaggi@users.noreply.github.com>
2020-01-19 22:22:19 -0800

    Update FirstRecurringCharacter.java

--- FirstRecurringCharacter.java
+++ FirstRecurringCharacter.java
@@ -9,3 +9,38 @@ Time:   O(n)
 Space:  O(n)
 */
 
+import java.util.Set;
+import java.util.HashSet;
+
+public class FirstRecurringCharacter{
+    
+    public static void main(String []args){
+        String str = "acbbac"; // return b;
+        String str2 = "abcdef"; // return null;
+        
+        System.out.println(findFirstRecurringCharacter(str));    
+        
+        System.out.println(findFirstRecurringCharacter(str2));    
+        
+    }
+    
+    private static Character findFirstRecurringCharacter(String str){
+        //if the string length is 0 or 1 there will not be recurring characters
+        // so we return a null
+        if(str.length() <= 1) return null;
+        
+        //contains the all the seen characters in the string
+        Set<Character> seenCharacter = new HashSet<Character>();
+        
+        //checks to see if we have seen the current character or not
+        for(int i = 0; i < str.length(); i++){
+            if(seenCharacter.contains(str.charAt(i))){
+                return str.charAt(i);
+            }else{
+                seenCharacter.add(str.charAt(i));
+            }
+        }
+        
+        return null;
+    }
+}
