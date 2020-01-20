ac8a94d31324adfc27d7c5fe83186265bed92474
RobinNaggi <43588161+RobinNaggi@users.noreply.github.com>
2020-01-19 22:26:55 -0800

    Update FirstRecurringCharacter.java

--- FirstRecurringCharacter.java
+++ FirstRecurringCharacter.java
@@ -8,7 +8,9 @@ Given the string "abcdef", return null.
 Time:   O(n)
 Space:  O(n)
 
-
+Part 2
+Time:   O(n)
+Space:  O(1)
 */
 
 import java.util.Set;
@@ -47,10 +49,14 @@ public class FirstRecurringCharacter{
     }
     
     private static Character findFirstRecurringCharacterPart2(String str){
+        //if the string length is 0 or 1 there will not be recurring characters
+        // so we return a null
         if(str.length() <= 1) return null;
         
+        //contains the all the seen characters in the string
         int[] seenCharacter = new int[26];
         
+        //checks to see if we have seen the current character or not
         for(int i = 0; i < str.length(); i++){
             if(seenCharacter[str.charAt(i) - 'a'] != 0){
                 return str.charAt(i);
