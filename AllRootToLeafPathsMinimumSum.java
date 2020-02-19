a53bd570fe6a926ae23f5948901e669e84e4bae9
RobinNaggi <43588161+RobinNaggi@users.noreply.github.com>
2020-02-18 18:27:12 -0800

    Update AllRootToLeafPathsMinimumSum.java

--- AllRootToLeafPathsMinimumSum.java
+++ AllRootToLeafPathsMinimumSum.java
@@ -1,7 +1,7 @@
 /*
 Given a binary tree, find a minimum path sum from root to a leaf.
 
-For example, the minimum path in this tree is [10, 5, 2, 1, -1], which has sum 15.
+For example, the minimum path in this tree is [10, 5, 1, -1], which has sum 15.
 
   10
  /  \
